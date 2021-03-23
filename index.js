b3583aaa872e0b47ce969c1c50a5d796f9adc470
myhaikudream <myhaikudream@yahoo.com>
2021-03-22 18:11:38 -0700

    commit tree 0f71147e1c91190c9e06234a37f090e8632a44b7

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(string) {
+  const time = parseInt(string, 10);
+  if ( time < 12) return "Good Morning";
+  if ( time > 17) return "Good Evening";
+  return "Good Afternoon";
+}
+
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
